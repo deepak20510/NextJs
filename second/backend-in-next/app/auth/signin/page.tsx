6ed45af31c994ea408a797fda6278b82160b99ef
Deepak Mahato <deepakmahato20510@gmail.com>
2026-05-08 20:23:41 +0530

    second folder added

---
+++ second/backend-in-next/app/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function User() {
+  return (
+    <div className="justify-between p-50 text-center items-center flex-5 ">
+      SignIn Page
+      <input type="text" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <button>Sign in</button>
+    </div>
+  );
+}
